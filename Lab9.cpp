f85ac767518ec80c9dd642b4ef1a638cea316175
Daniil Kylik <daniel.kulyk2004@gmail.com>
2022-12-12 16:55:53 +0300

    new file

---
+++ Lab9.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const int n = 10;
+	int randomDigits[n]{};
+	for (int i = 0; i < 10; i++)
+	{
+		randomDigits[i] = rand();
+		cout << randomDigits[i] << endl;
+	}
+	int N;
+	cout << "p >>";
+	cin >> N;
+
+	int index = 0;
+	for (int i = 0; i < n; ++i)
+		if (randomDigits[i] > N)
+		{
+			index++;
+		}
+	cout << endl;
+	if (index > 0)
+	{
+		cout << index << "\n";
+	}
+	else
+		cout << " Elements whose value is greater than " << N << "not found" << endl;
+	return 0;
+}
