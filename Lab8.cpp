37b5daf8cb5e9f4c96b01ad761ba13323015cff8
Daniil Kylik <daniel.kulyk2004@gmail.com>
2022-12-12 15:40:45 +0300

    change in file Lab8

--- Lab8.cpp
+++ Lab8.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
-#include <iomanip>
+#include <math.h>
+
 using namespace std;
 
 int main()
 {
-    const int n = 3;
-    const int m = 3;
-    int b[n][m] = { {22,6,8},{20, 12, 14},{16,18,10} };
-    int min_i = 0;
-    for (int i = 0; i < n; i++)
-        if (b[i][i] > b[min_i][min_i])
-        {
-            min_i = i;
-        }
-    cout << b[min_i][min_i] << endl;
+	float y, x, z;
+	cout << "Enter y, x, z: " << endl;
+	cin >> y >> x >> z;
+	double a = abs((y - sqrt(abs(x))) * (x - ((y)) / (z + pow(x, 4) / 4)));
+	cout << "A = " << a << endl;
 }
